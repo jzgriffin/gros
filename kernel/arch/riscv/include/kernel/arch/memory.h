1ccb53675a494122d1aa608e387f41e8feea5879
Jeremiah Griffin <jeremiah@griffin.to>
2023-06-05 21:51:19 -0700

    kernel: riscv: Use LITERAL/BIT in memory.h

--- kernel/arch/riscv/include/kernel/arch/memory.h
+++ kernel/arch/riscv/include/kernel/arch/memory.h
@@ -25,25 +25,25 @@
 #include <kernel/config.h>
 
 #if RISCV_MMU_BARE
-    #define KERNEL_BASE DRAM_BASE
+    #define KERNEL_BASE LITERAL_UX(DRAM_BASE)
 #else
     #if RISCV_MMU_SV32
         #error MMU Sv32 is not implemented
     #elif RISCV_MMU_SV39  // Two 38-bit halves.
-        #define USER_SPACE_BASE   0x0000000000000000
-        #define USER_SPACE_SIZE   0x0000004000000000
-        #define KERNEL_SPACE_BASE 0xFFFFFFC000000000
-        #define KERNEL_SPACE_SIZE 0x0000004000000000
+        #define USER_SPACE_BASE   LITERAL_UX(0x0000000000000000)
+        #define USER_SPACE_SIZE   LITERAL_UX(0x0000004000000000)
+        #define KERNEL_SPACE_BASE LITERAL_UX(0xFFFFFFC000000000)
+        #define KERNEL_SPACE_SIZE LITERAL_UX(0x0000004000000000)
     #elif RISCV_MMU_SV48
         #error MMU Sv48 is not implemented
     #else
         #error Unknown MMU
     #endif
-    #define KERNEL_BASE KERNEL_SPACE_BASE + DRAM_BASE
+    #define KERNEL_BASE LITERAL_UX(KERNEL_SPACE_BASE) + LITERAL_UX(DRAM_BASE)
 #endif
 
-#define PAGE_BITS 12
-#define PAGE_SIZE (1 << PAGE_BITS)
+#define PAGE_BITS LITERAL_UX(12)
+#define PAGE_SIZE BIT_UX(PAGE_BITS)
 #define PAGE_MASK (~(PAGE_SIZE - 1))
 
 #define ROUND_PAGE_DOWN(x) (((x) >> PAGE_BITS) << PAGE_BITS)
